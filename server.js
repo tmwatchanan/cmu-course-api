934b2ce2a66e5cafd500bb6a4a998e89314836c2
tmwatchanan <tmwatchanan@gmail.com>
2017-12-31 00:22:43 +0700

    Add web scraping for returning course title per request

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ var cheerio = require('cheerio');
 var app = express();
 
 app.get('/course', function (req, res) {
+    url = 'https://mis.cmu.ac.th/tqf/coursepublic.aspx?courseno=' + req.param.courseNo + '&semester=2&year=2560';
+    request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
 
-})
+            $('#lblCourseTitleEng').filter(function(){
+                var data = $(this);
+                var courseTitleEng = data.text();
+                return res.json(courseTitleEng);
+            })
+        }
+    })
+});
 
-app.listen('8081')
-
+app.listen('8081');
 console.log('Listening requests on port 8081');
-
 exports = module.exports = app;
