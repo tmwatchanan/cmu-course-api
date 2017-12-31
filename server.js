ce3b48b753e437de7cb7d8c3d4d056d11e38f431
tmwatchanan <tmwatchanan@gmail.com>
2017-12-31 17:10:32 +0700

    Enable CORS

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ var app = express();
 
 var port = process.env.PORT || 8080; // process.env.PORT lets the port be set by Heroku
 
+// Allow CORS
+var cors = require('cors');
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get('/course/:courseNo', function (req, res) {
     url = 'https://mis.cmu.ac.th/tqf/coursepublic.aspx?courseno=' + req.params.courseNo + '&semester=2&year=2560';
     request(url, function(error, response, html){
