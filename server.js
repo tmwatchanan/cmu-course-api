cc851e20023d7b68091f97e463c923437711201d
tmwatchanan <tmwatchanan@gmail.com>
2018-01-02 00:04:04 +0700

    Add fetching from public enrolled course list

--- server.js
+++ server.js
@@ -19,25 +19,108 @@ app.use(function (req, res, next) {
 
 app.get('/course/:courseNo', function (req, res) {
     url = 'https://mis.cmu.ac.th/tqf/coursepublic.aspx?courseno=' + req.params.courseNo + '&semester=2&year=2560';
-    request(url, function(error, response, html){
-        if(!error){
+    request(url, function (error, response, html) {
+        if (!error) {
             var $ = cheerio.load(html);
             var responseJson = {
                 courseName: '',
                 courseCredit: ''
             };
-            $('#lblCourseTitleEng').filter(function(){
+            $('#lblCourseTitleEng').filter(function () {
                 responseJson.courseName = $(this).text();
             });
-            $('#lblCredit').filter(function(){
+            $('#lblCredit').filter(function () {
                 responseJson.courseCredit = $(this).text().charAt(0);
             });
+
             return res.json(responseJson);
         }
-    })
+    });
 });
 
-app.listen(port, function() {
+app.get('/student', function (req, res) {
+    url = 'https://www3.reg.cmu.ac.th/regist' + req.query.semester + req.query.year.substring(req.query.year.length - 2) + '/public/result.php?id=' + req.query.id;
+    console.log(url);
+    request(url, function (error, response, html) {
+        if (!error) {
+            var $ = cheerio.load(html);
+
+            var courses = [];
+
+            $('tr.msan8').slice(2).each(function () {
+                let courseInformation = [];
+                $('td', this).each(function () {
+                    var value = $(this).text().trim();
+                    courseInformation.push(value);
+                });
+                courses.push(courseInformation);
+            });
+
+            var coursesJson = {
+                courseList: []
+            };
+
+            courses.forEach((course, index) => {
+                const courseInformation = {
+                    no: course[0],
+                    courseNo: course[1],
+                    title: course[2],
+                    sectionLec: course[3],
+                    sectionLab: course[4],
+                    creditLec: course[5],
+                    creditLab: course[6],
+                    day: course[7],
+                    time: course[8],
+                    type: course[9],
+                    grade: course[10]
+                };
+                coursesJson.courseList.push(courseInformation);
+            });
+            return res.json(coursesJson);
+        }
+    });
+});
+
+app.get('/cgpa-calculator', function (req, res) {
+    url = 'https://www3.reg.cmu.ac.th/regist' + req.query.semester + req.query.year.substring(req.query.year.length - 2) + '/public/result.php?id=' + req.query.id;
+    console.log(url);
+    request(url, function (error, response, html) {
+        if (!error) {
+            var $ = cheerio.load(html);
+
+            var courses = [];
+
+            $('tr.msan8').slice(2).each(function () {
+                let courseInformation = [];
+                $('td', this).each(function () {
+                    var value = $(this).text().trim();
+                    courseInformation.push(value);
+                });
+                courses.push(courseInformation);
+            });
+
+            var coursesJson = {
+                courseList: []
+            };
+
+            courses.forEach((course, index) => {
+                const creditLec = course[5].charAt(0),
+                    creditLab = course[6].charAt(0),
+                    type = course[9];
+                const courseInformation = {
+                    no: course[0],
+                    courseNo: course[1],
+                    title: course[2],
+                    credit: (creditLec == "0" ? creditLab : creditLec)
+                };
+                coursesJson.courseList.push(courseInformation);
+            });
+            return res.json(coursesJson);
+        }
+    });
+});
+
+app.listen(port, function () {
     console.log('Listening requests on port ' + port);
 });
 exports = module.exports = app;
